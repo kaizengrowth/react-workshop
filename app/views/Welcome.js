c2d3d29222b6042e93f95df7b819fc388bbd0520
Eric Garside <garside@gmail.com>
2016-06-27 12:55:25 -0400

    Clearing out tiles

--- app/views/Welcome.js
+++ app/views/Welcome.js
@@ -1,103 +1,12 @@
 import React, { Component } from 'react'
+import { Hex } from '../views'
 
 export default class Welcome extends Component {
   render() {
-    return <section id="welcome">
+    return <section id="welcome" className="content-area">
       <div className="g-row">
         <div className="g-column small-12">
-          <div className="hex-map">
-            <div className="hex-row u-clearfix">
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-            </div>
-            <div className="hex-row u-clearfix">
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-            </div>
-            <div className="hex-row u-clearfix">
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-            </div>
-            <div className="hex-row u-clearfix">
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-            </div>
-            <div className="hex-row u-clearfix">
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-            </div>
-            <div className="hex-row u-clearfix">
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-            </div>
-            <div className="hex-row u-clearfix">
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-01"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-              <div className="hex-tile grass-02"></div>
-              <div className="hex-tile grass-03"></div>
-            </div>
-          </div>
+          <h1>Hello, World!</h1>
         </div>
       </div>
     </section>
